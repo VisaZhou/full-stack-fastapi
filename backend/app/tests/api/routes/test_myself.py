de06f38303dcbb3178cf2d60c127b9330985635b
VisaZhou <472493922@qq.com>
2025-02-08 16:57:54 +0800

    【部署】修改workflow

--- backend/app/tests/api/routes/test_myself.py
+++ backend/app/tests/api/routes/test_myself.py
@@ -1,11 +1,8 @@
 # 检查并自动修复可修复的代码问题，如导入顺序、空格等：ruff check app/tests/mytest.py --fix
 # 进行代码格式化，它会调整代码样式使其符合统一的格式标准：ruff format app/tests/mytest.py
-from fastapi.testclient import TestClient
-
-from app.tests.utils.utils import get_superuser_token_headers
 
 
-def test_superuser_token_headers(client: TestClient) -> dict[str, str]:
-    headers = get_superuser_token_headers(client)
+def test_superuser_token_headers(superuser_token_headers: dict[str, str]) -> None:
+    headers = superuser_token_headers
     assert "Authorization" in headers
     assert headers["Authorization"].startswith("Bearer ")
--- backend/scripts/prestart.sh
+++ backend/scripts/prestart.sh
@@ -6,7 +6,7 @@ set -x
 # 这行命令会执行 app/backend_pre_start.py 脚本，通常它用于在数据库或其他服务启动时进行必要的前期操作。例如，它可能会初始化数据库连接、检查数据库是否可用、执行一些启动任务等。
 python app/backend_pre_start.py
 
-# 此命令通过 Alembic 进行数据库迁移。Alembic 是一个用于管理数据库版本控制的工具。upgrade head 命令会将数据库迁移到最新的版本。
+# 此命令通过 Alembic 进行数据库迁移，类似于flyway,/backend/app/alembic/versions/目录中存放了所有的数据库迁移脚本，通过 alembic upgrade head 命令可以将数据库迁移到最新版本。
 alembic upgrade head
 
 # 这行命令会执行 app/initial_data.py 脚本，通常用于向数据库插入一些初始数据，比如默认的用户、角色、配置设置等。
